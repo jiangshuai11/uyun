f86e49cd7fca3e3ce658768a97ba813effb1d14d
jiangshuai11 <45331626+jiangshuai11@users.noreply.github.com>
2020-10-03 16:51:52 +0800

    Update Test.java

--- cloud-api-commons/src/main/java/com/uyun/common/Test.java
+++ cloud-api-commons/src/main/java/com/uyun/common/Test.java
@@ -8,6 +8,6 @@ package com.uyun.common;
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("你好 master111111");
+        System.out.println("你好 master=========================================");
     }
 }
