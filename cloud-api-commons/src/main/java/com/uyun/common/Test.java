9a3b727e08cf87ef3069a5ac381787a0da843f51
jiangshuai <jiangshuai@broada.com>
2020-10-03 17:15:47 +0800

    1111

---
+++ cloud-api-commons/src/main/java/com/uyun/common/Test.java
@@ -0,0 +1,19 @@
+package com.uyun.common;
+
+/**
+ * @program: uyun
+ * @description:
+ * @author: 姜帅
+ * @create: 2020-10-03 16:20
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("你好 master123456789");
+        System.out.println("你好 master123456789");
+        System.out.println("你好 master123456789");
+        System.out.println("你好 master123456789");
+        System.out.println("你好 master123456789");
+        System.out.println("你好 master123456789");
+        System.out.println("你好 master123456789");
+    }
+}
